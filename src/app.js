f1241b99f061cf4e9ac6c78c2e587a399e33e82c
mushtaq-khan <70661816+mushtaq-khan@users.noreply.github.com>
2023-03-31 20:26:56 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const router = require('./routes/product');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.json());
 app.use(cors());
